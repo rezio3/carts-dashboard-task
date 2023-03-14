c518dc5906e6044dd1bd905d5ea5664161b1a4fe
Joszko <jakub.rezler96@gmail.com>
2023-03-14 21:42:29 +0100

    fetch carts data in SelectedCarts.jsx

--- src/components/SelectedCarts.jsx
+++ src/components/SelectedCarts.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import SingleCart from "./SingleCart";
+import { CartsContext } from "./context/CartsContext";
 
 const SelectedCarts = () => {
-	return <div className="list-of-carts"></div>;
+	const [carts, setCarts] = useContext(CartsContext);
+
+	useEffect(() => {
+		getData();
+	}, []);
+	const getData = async () => {
+		let cartData = await fetch("https://dummyjson.com/carts")
+			.then((res) => res.json())
+			.then((data) => {
+				return data;
+			});
+		// console.log(cartData.carts.length);
+		// setState(true);
+		setCarts({
+			...carts,
+			selectedCarts: cartData.carts,
+		});
+	};
+
+	if (carts.selectedCarts) {
+		// console.log(state.data);
+		return (
+			<div className="list-of-carts">
+				<h2>Selected Carts</h2>
+				{carts.selectedCarts.map((e) => {
+					return (
+						<SingleCart
+							name={`Cart ${e.id}`}
+							id={e.id}
+							deleteItem={handleDeleteCart}
+						/>
+					);
+				})}
+			</div>
+		);
+	} else {
+		return (
+			<div className="list-of-carts">
+				<h2>Selected Carts</h2>
+				<p>Loading...</p>
+			</div>
+		);
+	}
 };
 
 export default SelectedCarts;
