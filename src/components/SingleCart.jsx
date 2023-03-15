7a6e564c29e4adbc97f5240f945da46846ab81b2
Joszko <jakub.rezler96@gmail.com>
2023-03-15 14:46:12 +0100

    add chart icon and update styles

--- src/components/SingleCart.jsx
+++ src/components/SingleCart.jsx
@@ -3,19 +3,32 @@ import "../style/css/SingleCart.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
 
 const SingleCart = (props) => {
+	const showChart = (e) => {
+		console.log(e.target.id);
+	};
 	return (
-		<div className="cart" data-id={props.id}>
+		<div className="cart">
 			<span>{props.name}</span>
 
-			<button id={props.id} onClick={props.cartSwitch}>
-				{props.list === "added" ? (
-					<FontAwesomeIcon icon={faTrash} className="icon" />
-				) : (
-					<FontAwesomeIcon icon={faPlus} className="icon" />
-				)}
-			</button>
+			<div className="buttons-container">
+				<button
+					name={props.id}
+					onClick={props.cartSwitch}
+					className="trash-button"
+				>
+					{props.list === "added" ? (
+						<FontAwesomeIcon icon={faTrash} className="trash-icon" />
+					) : (
+						<FontAwesomeIcon icon={faPlus} className="trash-icon" />
+					)}
+				</button>
+				<button name={props.id} onClick={showChart} className="chart-button">
+					<FontAwesomeIcon icon={faChartSimple} className="chart-icon" />
+				</button>
+			</div>
 		</div>
 	);
 };
