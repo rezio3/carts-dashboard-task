c0d6b76e5fb1ceda544d198d72156f0000e5fee6
Joszko <jakub.rezler96@gmail.com>
2023-03-15 02:01:37 +0100

    add fontawesome icons and style SingleCart

--- src/components/SingleCart.jsx
+++ src/components/SingleCart.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "../style/css/SingleCart.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const SingleCart = (props) => {
 	return (
 		<div className="cart" data-id={props.id}>
 			<span>{props.name}</span>
+
 			<button id={props.id} onClick={props.cartSwitch}>
-				X
+				{props.list === "added" ? (
+					<FontAwesomeIcon icon={faTrash} className="icon" />
+				) : (
+					<FontAwesomeIcon icon={faPlus} className="icon" />
+				)}
 			</button>
 		</div>
 	);
