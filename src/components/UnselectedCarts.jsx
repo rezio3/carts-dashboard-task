fa981e14ded6ca7c029317e62e37deb3349913ec
Joszko <jakub.rezler96@gmail.com>
2023-03-15 01:06:05 +0100

    create add-cart handler

--- src/components/UnselectedCarts.jsx
+++ src/components/UnselectedCarts.jsx
@@ -4,12 +4,50 @@ import SingleCart from "./SingleCart";
 
 const UnselectedCarts = () => {
 	const [carts, setCarts] = useContext(CartsContext);
-	console.log(carts.unselectedCarts);
+	// console.log(carts.unselectedCarts);
+	const handleAddCart = async (e) => {
+		// console.log(e.target.id);
+
+		const addedElement = await fetch(
+			`https://dummyjson.com/carts/${e.target.id}`
+		)
+			.then((res) => res.json())
+			.then((e) => {
+				return e;
+			});
+		carts.unselectedCarts.map((el) => {
+			if (el.id === Number(e.target.id)) {
+				const indexToDelete = carts.unselectedCarts.indexOf(el);
+				console.log(indexToDelete);
+				setCarts({
+					...carts,
+					unselectedCarts: [carts.unselectedCarts.splice(indexToDelete, 1)],
+				});
+				return el;
+			}
+		});
+		const updatedArray = [...carts.selectedCarts, addedElement];
+		const sortedArray = updatedArray.sort(function (a, b) {
+			return a.id - b.id;
+		});
+		setCarts({
+			...carts,
+			selectedCarts: sortedArray,
+		});
+	};
+
 	return (
 		<div className="list-of-carts">
 			<h2>Unselected Carts</h2>
 			{carts.unselectedCarts.map((e) => {
-				return <SingleCart key={e.id} name={`Cart ${e.id}`} />;
+				return (
+					<SingleCart
+						key={e.id}
+						name={`Cart ${e.id}`}
+						id={e.id}
+						cartSwitch={handleAddCart}
+					/>
+				);
 			})}
 		</div>
 	);
